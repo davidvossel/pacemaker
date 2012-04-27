fa10f9250d565172e90be48cf9538a23202d0cdf
David Vossel <dvossel@redhat.com>
2012-04-27 10:41:30 -0500

    Introducing the new lrmd and client library

---
+++ lib/lrmd/lrmd_client.c
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2012 David Vossel <dvossel@redhat.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+
+#include <crm_internal.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+#include <ctype.h>
+
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#include <glib.h>
+#include <dirent.h>
+
+#include <crm/lrmd.h>
+
+lrmd_t *lrmd_api_new(void)
+{
+	return NULL;
+}
+
+void lrmd_api_delete(lrmd_t * lrm)
+{
+
+}
---
+++ lrmd/main.c
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2012 David Vossel <dvossel@redhat.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+
+#include <crm_internal.h>
+
+#include <sys/param.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <sys/utsname.h>
+
+#include <stdlib.h>
+#include <errno.h>
+#include <fcntl.h>
+
+#include <crm/crm.h>
+#include <crm/msg_xml.h>
+#include <crm/common/ipc.h>
+#include <crm/common/cluster.h>
+#include <crm/common/util.h>
+
+#include <crm/common/xml.h>
+#include <crm/common/msg.h>
+
+#include <crm/cib.h>
+
+int main(int argc, char ** argv)
+{
+	int rc = 0;
+
+	crm_log_init("lrmd-ng", LOG_INFO, TRUE, FALSE, argc, argv);
+	crm_info("LRMD init");
+
+	return rc;
+}
