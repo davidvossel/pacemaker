4dc6ba610cf4b4b6aa67f4995b6649fd25c9b063
David Vossel <dvossel@redhat.com>
2012-05-07 10:02:51 -0500

    Formatting changes

--- include/crm/lrmd.h
+++ include/crm/lrmd.h
@@ -24,39 +24,39 @@ typedef struct lrmd_s lrmd_t;
 typedef struct lrmd_key_value_s lrmd_key_value_t;
 struct lrmd_key_value_t;
 
-#define F_LRMD_OPERATION		"lrmd_op"
-#define F_LRMD_CLIENTNAME		"lrmd_clientname"
-#define F_LRMD_CLIENTID		"lrmd_clientid"
+#define F_LRMD_OPERATION        "lrmd_op"
+#define F_LRMD_CLIENTNAME       "lrmd_clientname"
+#define F_LRMD_CLIENTID         "lrmd_clientid"
 #define F_LRMD_CALLBACK_TOKEN	"lrmd_async_id"
-#define F_LRMD_CALLID		"lrmd_callid"
-#define F_LRMD_CALLOPTS		"lrmd_callopt"
-#define F_LRMD_CALLDATA		"lrmd_calldata"
-#define F_LRMD_RC			"lrmd_rc"
-#define F_LRMD_EXEC_RC			"lrmd_exec_rc"
-#define F_LRMD_TIMEOUT		"lrmd_timeout"
-#define F_LRMD_CLASS		"lrmd_class"
-#define F_LRMD_PROVIDER		"lrmd_provider"
-#define F_LRMD_TYPE		"lrmd_type"
-#define F_LRMD_ORIGIN		"lrmd_origin"
-#define F_LRMD_RSC_ID	"lrmd_rsc_id"
-#define F_LRMD_RSC_CMD_ID	"lrmd_rsc_cmd_id"
-#define F_LRMD_RSC_ACTION	"lrmd_rsc_action"
-#define F_LRMD_RSC_START_DELAY	"lrmd_rsc_start_delay"
-#define F_LRMD_RSC_INTERVAL	"lrmd_rsc_interval"
-#define F_LRMD_RSC_TIMEOUT	"lrmd_rsc_timeout"
-#define F_LRMD_RSC_METADATA	"lrmd_rsc_metadata_res"
+#define F_LRMD_CALLID           "lrmd_callid"
+#define F_LRMD_CALLOPTS         "lrmd_callopt"
+#define F_LRMD_CALLDATA         "lrmd_calldata"
+#define F_LRMD_RC               "lrmd_rc"
+#define F_LRMD_EXEC_RC          "lrmd_exec_rc"
+#define F_LRMD_TIMEOUT          "lrmd_timeout"
+#define F_LRMD_CLASS            "lrmd_class"
+#define F_LRMD_PROVIDER         "lrmd_provider"
+#define F_LRMD_TYPE             "lrmd_type"
+#define F_LRMD_ORIGIN           "lrmd_origin"
+#define F_LRMD_RSC_ID           "lrmd_rsc_id"
+#define F_LRMD_RSC_CMD_ID       "lrmd_rsc_cmd_id"
+#define F_LRMD_RSC_ACTION       "lrmd_rsc_action"
+#define F_LRMD_RSC_START_DELAY  "lrmd_rsc_start_delay"
+#define F_LRMD_RSC_INTERVAL     "lrmd_rsc_interval"
+#define F_LRMD_RSC_TIMEOUT      "lrmd_rsc_timeout"
+#define F_LRMD_RSC_METADATA     "lrmd_rsc_metadata_res"
+#define F_LRMD_RSC              "lrmd_rsc"
 
-#define LRMD_OP_RSC_REG	"lrmd_rsc_register"
-#define LRMD_OP_RSC_EXEC	"lrmd_rsc_exec"
-#define LRMD_OP_RSC_UNREG	"lrmd_rsc_unregister"
-#define LRMD_OP_RSC_CALL	"lrmd_rsc_call"
-#define LRMD_OP_RSC_GET_METADATA	"lrmd_rsc_metadata"
+#define LRMD_OP_RSC_REG           "lrmd_rsc_register"
+#define LRMD_OP_RSC_EXEC          "lrmd_rsc_exec"
+#define LRMD_OP_RSC_UNREG         "lrmd_rsc_unregister"
+#define LRMD_OP_RSC_CALL          "lrmd_rsc_call"
+#define LRMD_OP_RSC_GET_METADATA  "lrmd_rsc_metadata"
 
-#define F_LRMD_RSC	"lrmd_rsc"
 
-#define T_LRMD		"lrmd"
-#define T_LRMD_REPLY	"lrmd_reply"
-#define T_LRMD_NOTIFY	"lrmd_notify"
+#define T_LRMD           "lrmd"
+#define T_LRMD_REPLY     "lrmd_reply"
+#define T_LRMD_NOTIFY    "lrmd_notify"
 
 extern lrmd_t *lrmd_api_new(void);
 extern void lrmd_api_delete(lrmd_t * lrmd);
@@ -64,30 +64,30 @@ extern lrmd_key_value_t *lrmd_key_value_add(lrmd_key_value_t *kvp,
 	const char *key,
 	const char *value);
 
+/* Reserved for future use */
 enum lrmd_call_options {
 	lrmd_opt_none		= 0x00000000,
-	// lrmd_opt_sync_call	= 0x00000001, 
 };
 
 enum lrmd_errors {
-	lrmd_ok						=  0,
-	lrmd_pending				= -1,
-	lrmd_err_generic			= -2,
-	lrmd_err_internal			= -3,
-	lrmd_err_not_supported		= -4,
-	lrmd_err_connection			= -5,
-	lrmd_err_missing			= -6,
-	lrmd_err_exists				= -7,
-	lrmd_err_timeout			= -8,
-	lrmd_err_ipc				= -9,
-	lrmd_err_peer				= -10,
-	lrmd_err_unknown_operation	= -11,
-	lrmd_err_unknown_rsc		= -12,
-	lrmd_err_none_available		= -13,
-	lrmd_err_authentication		= -14,
-	lrmd_err_signal				= -15,
-	/* command failed to execute */
-	lrmd_err_exec_failed		= -16,
+	lrmd_ok                      =  0,
+	lrmd_pending                 = -1,
+	lrmd_err_generic             = -2,
+	lrmd_err_internal            = -3,
+	lrmd_err_not_supported       = -4,
+	lrmd_err_connection          = -5,
+	lrmd_err_missing             = -6,
+	lrmd_err_exists              = -7,
+	lrmd_err_timeout             = -8,
+	lrmd_err_ipc                 = -9,
+	lrmd_err_peer                = -10,
+	lrmd_err_unknown_operation   = -11,
+	lrmd_err_unknown_rsc         = -12,
+	lrmd_err_none_available      = -13,
+	lrmd_err_authentication      = -14,
+	lrmd_err_signal              = -15,
+	/* command failed to execute, do not expect notify event to occur. */
+	lrmd_err_exec_failed         = -16,
 };
 
 enum lrmd_callback_event {
@@ -138,6 +138,7 @@ typedef struct lrmd_api_operations_s
 
 	/*!
 	 * \brief Issue a command on a resource
+	 *
 	 * \retval call_id to track async event result on success
 	 * \retval negative error code or failure
 	 */
@@ -155,9 +156,8 @@ typedef struct lrmd_api_operations_s
 	 * \brief Cancel a recurring command.
 	 *
 	 * \retval 0, cancel command sent.
-	 * \retval -1, cancel command failed.
 	 */
-	/* TODO IMPLEMENT add callback for cancel. */
+	/* TODO IMPLEMENT */
 	int (*cancel)(lrmd_t *lrmd, const char *call_id);
 
 } lrmd_api_operations_t;
